c5d505fede9a4a4d5b90735f5ef862767e4dfe27
Dinh Y Thuc <dinhythuc240594@gmail.com>
2026-06-03 19:31:17 +0700

    remove .next folder

--- frontend/.next/dev/server/app/api/locations/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/locations/route.js")
-R.c("server/chunks/src_app_api_locations_route_ts_0~o4cvr._.js")
-R.c("server/chunks/node_modules_next_132u1c1._.js")
-R.c("server/chunks/[root-of-the-server]__0zzac6j._.js")
-R.c("server/chunks/_next-internal_server_app_api_locations_route_actions_08a_do5.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/locations/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/locations/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
